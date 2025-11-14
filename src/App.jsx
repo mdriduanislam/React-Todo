9a1d97f5dcdb100324f73b777224682f72177fdc
mdriduanislam <mohhammadris786@gmail.com>
2025-11-15 04:17:26 +0600

    added some cool ui and search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
     if (filter === 'completed') return todo.completed;
     return true;
   })
+  const [search, setSearch] = useState('');
+
   const addTodo = (text) => {
     const newTodos = {
       id: Date.now(),
@@ -31,38 +33,49 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id))
   }
 
-  const editTodo = (id, newText) =>{
-    setTodos(todos.map((todo)=>todo.id === id? {...todo, text:newText} : todo))
+  const editTodo = (id, newText) => {
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, text: newText } : todo))
   }
+
+  const searchTodo = filteredTodos.filter(
+    todo => todo.text.toLowerCase().includes(search.toLowerCase()))
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos])
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        📝My todo App
-      </h1>
-      <TodoForm addTodo={addTodo} />
-      <div>
-        <button onClick={() => setFilter('all')}
-          style={{ fontWeight: filter === 'all' ? 'bold' : 'normal' }}
-        >
-          All
-        </button>
-        <button onClick={() => setFilter('active')}
-          style={{ fontWeight: filter === 'active' ? 'bold' : 'normal' }}
-        >
-          Active
-        </button>
-        <button onClick={() => setFilter('completed')}
-          style={{ fontWeight: filter === 'completed' ? 'bold' : 'normal' }}
-        >
-          Completed
-        </button>
+    <div className='bg-blue-950 flex justify-center items-center w-full h-screen'>
+      <div className='bg-blue-900 shadow-2xl p-20 rounded-3xl h-[90%] w-[70%] flex flex-col gap-3 justify-center items-center'> 
+        <h1 className='text-3xl font-bold text-center pb-5' >
+          My Todo App
+        </h1>
+        <TodoForm addTodo={addTodo}/>
+        <input className='bg-blue-500 rounded-lg w-[50%] h-10 outline-none p-2'
+          type="text"
+          placeholder='Seach your todos...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className='w-[50%] h-10 flex gap-2 justify-around items-center' >
+          <button className='bg-blue-500 rounded-lg w-[33%] h-full' onClick={() => setFilter('all')}
+            style={{ fontWeight: filter === 'all' ? 'bold' : 'normal' }}
+          >
+            All
+          </button>
+          <button className='bg-blue-500 rounded-lg w-[33%] h-full' onClick={() => setFilter('active')}
+            style={{ fontWeight: filter === 'active' ? 'bold' : 'normal' }}
+          >
+            Active
+          </button>
+          <button className='bg-blue-500 rounded-lg w-[33%] h-full' onClick={() => setFilter('completed')}
+            style={{ fontWeight: filter === 'completed' ? 'bold' : 'normal' }}
+          >
+            Completed
+          </button>
+        </div>
+        <TodoList todos={searchTodo} toggleTodo={toggleTodo} removeTodo={removeTodo} editTodo={editTodo} />
       </div>
-      <TodoList todos={filteredTodos} toggleTodo={toggleTodo} removeTodo={removeTodo} editTodo={editTodo}/>
-    </>
+    </div>
   )
 }
 
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -9,17 +9,15 @@ function TodoForm({ addTodo}) {
         setText("");
     }
     return (
-        <div>
-            <form action="" onClick={handleClick}>
-                <input
+            <form action="" onClick={handleClick} className='w-[50%] h-10 flex justify-center items-center gap-1.5'>
+                <input className='outline-none p-2 bg-blue-500 rounded-lg w-[80%]'
                     type="text"
                     value={text}
                     placeholder='Add a new task...'
                     onChange={(e) => setText(e.target.value)}
                 />
-                <button type="submit">Add</button>
+                <button type="submit" className='p-2 bg-blue-700 rounded-lg w-[20%] font-bold '>Add</button>
             </form>
-        </div >
     )
 }
 export default TodoForm;
